a014f88a9b55b8ac404ba73ba2deaf598909d7fc
Ding Huang <jeff0815jeff0815@hotmail.com>
2018-03-06 19:59:44 +0800

    Create lengthOfLongestSubstring.cpp
    The original code written by @Dinglet

---
+++ lengthOfLongestSubstring.cpp
@@ -0,0 +1,30 @@
+#include<unordered_map>
+#include<queue>
+using namespace std;
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s)
+    {
+        unordered_map<char, int> hashtable;
+        queue<char> candidateSubstring;
+        int countMaxLength = 0;
+
+        for(int i=0; i<s.length(); i+=1)
+        {
+            hashtable[s[i]] = hashtable[s[i]] + 1;
+            if(hashtable.size() > countMaxLength)
+                countMaxLength = hashtable.size();
+            candidateSubstring.push(s[i]);
+
+            if(candidateSubstring.size() > countMaxLength)
+            {
+                if(hashtable[candidateSubstring.front()] == 1)
+                    hashtable.erase(candidateSubstring.front());
+                else
+                    hashtable[candidateSubstring.front()] -= 1;
+                candidateSubstring.pop();
+            }
+        }
+        return countMaxLength;
+    }
+};
