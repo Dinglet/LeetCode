21966a6286df80ab35a7420b3746f5462ad5ac70
Ding Huang <jeff0815jeff0815@hotmail.com>
2018-03-07 16:37:04 +0800

    Update and rename lengthOfLongestSubstring.cpp to LongestSubstringWithoutRepeatingCharacters.cpp

--- LongestSubstringWithoutRepeatingCharacters.cpp
+++ LongestSubstringWithoutRepeatingCharacters.cpp
@@ -1,3 +1,7 @@
+/*
+Longest Substring Without Repeating Characters
+https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
+*/
 #include<unordered_map>
 #include<queue>
 using namespace std;
