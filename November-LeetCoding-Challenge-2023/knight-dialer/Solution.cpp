ac5ef974cc3985e196cb97ae94a6b25f6a4a9de7
Huang, Shien-Ting <jeff0815jeff0815@hotmail.com>
2023-11-27 15:31:41 +0800

    Update Solution.cpp
    1. Applied Approach 4: Efficient Iteration On States from the Editorial.
    2. Replace accumulate() by a series (the sum of 4 numbers).

--- November-LeetCoding-Challenge-2023/knight-dialer/Solution.cpp
+++ November-LeetCoding-Challenge-2023/knight-dialer/Solution.cpp
@@ -11,44 +11,44 @@ class Solution
 public:
     int knightDialer(int n)
     {
+        // Group 0: 0
         // 0: 4, 6
-        // 1: 6, 8
-        // 2: 7, 9
-        // 3: 4, 8
-        // 4: 0, 3, 9
-        // 5:
-        // 6: 0, 1, 7
-        // 7: 2, 6
-        // 8: 1, 3
-        // 9: 2, 4
 
-        uint64_t dp[10] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
-        uint64_t dp2n[10];
+        // Group 1: 1, 3, 7, 9
+        // 1: 6, 8
+        // 3: 4, 8
+        // 7: 2, 6
+        // 9: 2, 4
+        
+        // Group 2: 2, 8
+        // 2: 7, 9
+        // 8: 1, 3
+
+        // Group 3: 4, 6
+        // 4: 0, 3, 9
+        // 6: 0, 1, 7
+        
+        // Group 4: 5 (don't care)
+        // 5:
+
+        if (n == 1)
+            return 10;
+
+        uint64_t dp[4] = {1, 4, 2, 2};
+        uint64_t dp2n[4];
         for (int i = 1; i <= n/2; i++)
         {
-            dp2n[0] = (dp[4] + dp[6]) % MOD;
-            dp2n[1] = (dp[6] + dp[8]) % MOD;
-            dp2n[2] = (dp[7] + dp[9]) % MOD;
-            dp2n[3] = (dp[4] + dp[8]) % MOD;
-            dp2n[4] = (dp[0] + dp[3] + dp[9]) % MOD;
-            dp2n[5] = 0;
-            dp2n[6] = (dp[0] + dp[1] + dp[7]) % MOD;
-            dp2n[7] = (dp[2] + dp[6]) % MOD;
-            dp2n[8] = (dp[1] + dp[3]) % MOD;
-            dp2n[9] = (dp[2] + dp[4]) % MOD;
+            dp2n[0] = dp[3];
+            dp2n[1] = (dp[2] + dp[2] + dp[3] + dp[3]) % MOD;
+            dp2n[2] = dp[1];
+            dp2n[3] = (dp[0] + dp[0] + dp[1]) % MOD;
 
-            dp[0] = (dp2n[4] + dp2n[6]) % MOD;
-            dp[1] = (dp2n[6] + dp2n[8]) % MOD;
-            dp[2] = (dp2n[7] + dp2n[9]) % MOD;
-            dp[3] = (dp2n[4] + dp2n[8]) % MOD;
-            dp[4] = (dp2n[0] + dp2n[3] + dp2n[9]) % MOD;
-            dp[5] = 0;
-            dp[6] = (dp2n[0] + dp2n[1] + dp2n[7]) % MOD;
-            dp[7] = (dp2n[2] + dp2n[6]) % MOD;
-            dp[8] = (dp2n[1] + dp2n[3]) % MOD;
-            dp[9] = (dp2n[2] + dp2n[4]) % MOD;
+            dp[0] = dp2n[3];
+            dp[1] = (dp2n[2] + dp2n[2] + dp2n[3] + dp2n[3]) % MOD;
+            dp[2] = dp2n[1];
+            dp[3] = (dp2n[0] + dp2n[0] + dp2n[1]) % MOD;
         }
         
-        return (n&1) ? accumulate(dp, dp + 10, 0ULL) % MOD : accumulate(dp2n, dp2n + 10, 0ULL) % MOD;
+        return (n&1) ? (dp[0] + dp[1] + dp[2] + dp[3]) % MOD : (dp2n[0] + dp2n[1] + dp2n[2] + dp2n[3]) % MOD;
     }
 };
