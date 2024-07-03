5eadcb734b178a2fcdcc6817c1462965900455a5
Huang, Shien-Ting <10545636+Dinglet@users.noreply.github.com>
2024-07-03 12:47:41 +0800

    Add a test case

--- July-LeetCoding-Challenge-2024/minimum-difference-between-largest-and-smallest-value-in-three-moves/main.cpp
+++ July-LeetCoding-Challenge-2024/minimum-difference-between-largest-and-smallest-value-in-three-moves/main.cpp
@@ -25,6 +25,11 @@ int main(int argc, char const *argv[])
     assert((solution.minDifference(nums3) == 0));
     printf("\n");
 
+    printf("Test 4\n");
+    vector<int> nums4 = {511,526,975,111,71,913,238,734,256,351,454,312,24,502,165,322,154,567,756,63,386,982,900,202,530,598,962,464,950,936,123,591,270,688,479,426};
+    assert((solution.minDifference(nums4) == 871));
+    printf("\n");
+
     std::puts("All test cases passed.");
 
     return 0;
