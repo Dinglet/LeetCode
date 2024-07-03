91a8b1ed88388a8908e54f2423c53091950f3217
Huang, Shien-Ting <10545636+Dinglet@users.noreply.github.com>
2024-07-03 12:47:09 +0800

    Refactor code using nth_element and sort

--- July-LeetCoding-Challenge-2024/minimum-difference-between-largest-and-smallest-value-in-three-moves/Solution.hpp
+++ July-LeetCoding-Challenge-2024/minimum-difference-between-largest-and-smallest-value-in-three-moves/Solution.hpp
@@ -20,70 +20,20 @@ public:
             return 0;
         }
 
-        vector<int> mins = {kMax, kMax, kMax, kMax}; // 4 smallest elements
-        vector<int> maxs = {kMin, kMin, kMin, kMin}; // 4 largest elements
+        nth_element(nums.begin(), nums.begin() + 4, nums.end());
+        nth_element(nums.begin()+ 4, nums.end() - 4, nums.end());
+        sort(nums.begin(), nums.begin() + 4);
+        sort(nums.end() - 4, nums.end());
 
-        for (int n : nums)
-        {
-            if (n < mins[0])
-            {
-                mins[3] = mins[2];
-                mins[2] = mins[1];
-                mins[1] = mins[0];
-                mins[0] = n;
-            }
-            else if (n < mins[1])
-            {
-                mins[3] = mins[2];
-                mins[2] = mins[1];
-                mins[1] = n;
-            }
-            else if (n < mins[2])
-            {
-                mins[3] = mins[2];
-                mins[2] = n;
-            }
-            else if (n < mins[3])
-            {
-                mins[3] = n;
-            }
+        // printf("mins: %d %d %d %d\n", nums[0], nums[1], nums[2], nums[3]);
+        // printf("maxs: %d %d %d %d\n", *(nums.end() - 4), *(nums.end() - 3), *(nums.end() - 2), *(nums.end() - 1));
 
-            if (n > maxs[3])
-            {
-                maxs[0] = maxs[1];
-                maxs[1] = maxs[2];
-                maxs[2] = maxs[3];
-                maxs[3] = n;
-            }
-            else if (n > maxs[2])
-            {
-                maxs[0] = maxs[1];
-                maxs[1] = maxs[2];
-                maxs[2] = n;
-            }
-            else if (n > maxs[1])
-            {
-                maxs[0] = maxs[1];
-                maxs[1] = n;
-            }
-            else if (n > maxs[0])
-            {
-                maxs[0] = n;
-            }
-        }
-
-        // printf("mins: %d %d %d %d\n", mins[0], mins[1], mins[2], mins[3]);
-        // printf("maxs: %d %d %d %d\n", maxs[0], maxs[1], maxs[2], maxs[3]);
-
-        int result = maxs[0] - mins[0];
+        int result = *(nums.end() - 4) - *(nums.begin());
         for (int i = 1; i < 4; i++)
         {
-            result = min(result, maxs[i] - mins[i]);
+            result = min(result, *(nums.end() - 4 + i) - *(nums.begin() + i));
         }
 
         return result;
     }
-private:
-    const int kMin = -1e9;
-    const int kMax = 1e9;
 };
