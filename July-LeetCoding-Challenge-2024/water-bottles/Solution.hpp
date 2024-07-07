75c5702115e99737649c2eb5d4d998455014f2ac
Huang, Shien-Ting <10545636+Dinglet@users.noreply.github.com>
2024-07-07 10:19:24 +0800

    Reduce the time complexity with a O(1) approach

--- July-LeetCoding-Challenge-2024/water-bottles/Solution.hpp
+++ July-LeetCoding-Challenge-2024/water-bottles/Solution.hpp
@@ -5,21 +5,10 @@ class Solution
 public:
     int numWaterBottles(int numBottles, int numExchange)
     {
-        int remainder;
-        int countDrunk = 0;
-        while (numBottles >= numExchange)
-        {
-            // old bottles
-            remainder = numBottles % numExchange;
-            // new bottles
-            numBottles /= numExchange;
-            // count drunk
-            countDrunk += numBottles * numExchange;
-
-            numBottles += remainder;
-        }
-
-        // countDrunk + remainder
-        return countDrunk + numBottles;
+        // do an exchange to get 1 more bottle
+        //     numBottles = numBottles - (numExchange - 1)
+        // until numBottles < numExchange
+        //     i.e. (numBottles - 1) < (numExchange - 1)
+        return numBottles + (numBottles - 1) / (numExchange - 1);
     }
 };
