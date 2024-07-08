ec57d8d1679f81283aec8767cb170796ebd8e6f6
Huang, Shien-Ting <10545636+Dinglet@users.noreply.github.com>
2024-07-08 11:09:31 +0800

    Add the 8th challenge

---
+++ July-LeetCoding-Challenge-2024/find-the-winner-of-the-circular-game/Solution.hpp
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/find-the-winner-of-the-circular-game/
+
+#include <queue>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int findTheWinner(int n, int k)
+    {
+        queue<int> q;
+        for (int i = 1; i <= n; i++)
+        {
+            q.push(i);
+        }
+
+        while (q.size() > 1)
+        {
+            int t = k - 1;
+            while(t--)
+            {
+                q.push(q.front());
+                q.pop();
+            }
+            q.pop();
+        }
+
+        return q.front();
+    }
+};
