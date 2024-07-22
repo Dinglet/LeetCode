b129182d066875a71a731450e10fe58939b2c2f4
Huang, Shien-Ting <10545636+Dinglet@users.noreply.github.com>
2024-07-22 14:00:57 +0800

    Try sorting without a map's help

--- July-LeetCoding-Challenge-2024/sort-the-people/Solution.hpp
+++ July-LeetCoding-Challenge-2024/sort-the-people/Solution.hpp
@@ -1,6 +1,7 @@
 #include <vector>
 #include <string>
 #include <map>
+#include <algorithm>
 
 using namespace std;
 
@@ -9,17 +10,21 @@ class Solution
 public:
     vector<string> sortPeople(vector<string>& names, vector<int>& heights)
     {
-        // heights is unique
-        // order by height in descending order
-        map<int, string, greater<int>> heightToName;
-        for (int i = 0; i < names.size(); ++i)
+        // since 1 <= n <= 1000 and 1 <= heights[i] <= 100000,
+        // we can use higher bits of heights[i] to store the original index of heights[i]
+        const int kShift = 17;
+        const int kMask = 0x1FFFF;
+
+        for (uint16_t i = 0; i < heights.size(); ++i)
         {
-            heightToName[heights[i]] = names[i];
+            heights[i] += i << kShift;
         }
+        sort(heights.begin(), heights.end(), [&](int a, int b) { return (a & kMask) > (b & kMask); });
 
         vector<string> result;
-        for (auto& [height, name] : heightToName)
+        for (uint16_t i = 0; i < heights.size(); ++i)
         {
+            string name = names[(uint32_t)heights[i] >> kShift];
             result.push_back(name);
         }
 
