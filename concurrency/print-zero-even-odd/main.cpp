f0112472595ebf058b9fd4c7527e5d80b6b9b048
Huang, Shien-Ting <10545636+Dinglet@users.noreply.github.com>
2026-02-05 10:39:38 +0800

    Replace code for verifying solution to the Print Zero Even Odd problem

--- concurrency/print-zero-even-odd/main.cpp
+++ concurrency/print-zero-even-odd/main.cpp
@@ -4,9 +4,40 @@
 
 using namespace std;
 
+string printed;
+
+void clearPrinted()
+{
+    printed.clear();
+}
+
 void printNumber(int x)
 {
-    cout << x;
+    printed += to_string(x);
+}
+
+bool checkZeroEvenOdd(int n, string &message)
+{
+    clearPrinted();
+    ZeroEvenOdd zeo(n);
+    thread t1(&ZeroEvenOdd::zero, &zeo, printNumber);
+    thread t2(&ZeroEvenOdd::even, &zeo, printNumber);
+    thread t3(&ZeroEvenOdd::odd, &zeo, printNumber);
+    t1.join();
+    t2.join();
+    t3.join();
+
+    string expected;
+    for (int i = 1; i <= n; ++i)
+    {
+        expected += "0" + to_string(i);
+    }
+
+    message = "n = " + to_string(n) + "\n";
+    message += "printed:  " + printed + "\n";
+    message += "expected: " + expected + "\n";
+    message += (printed == expected ? "PASS\n" : "FAIL\n");
+    return printed == expected;
 }
 
 int main()
@@ -16,15 +47,9 @@ int main()
     {
         if (n <= 0)
             break;
-        ZeroEvenOdd zeo(n);
-        thread t1(&ZeroEvenOdd::zero, &zeo, printNumber);
-        thread t2(&ZeroEvenOdd::even, &zeo, printNumber);
-        thread t3(&ZeroEvenOdd::odd, &zeo, printNumber);
-        t1.join();
-        t2.join();
-        t3.join();
-
-        cout << endl;
+        string message;
+        bool result = checkZeroEvenOdd(n, message);
+        cout << message << endl;
     }
 
     return 0;
