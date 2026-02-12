c304d03847aa50f04744fe0603850387c0e386bf
Huang, Shien-Ting <10545636+Dinglet@users.noreply.github.com>
2026-02-12 10:16:17 +0800

    Reuse count of letters to reduce execution time

--- daily-question/longest-balanced-substring-i/Solution.hpp
+++ daily-question/longest-balanced-substring-i/Solution.hpp
@@ -9,14 +9,17 @@ class Solution
 public:
     int longestBalanced(string s)
     {
-        for (int len = s.size(); len > 0; --len)
-        {
-            vector<int> count_letters(26);
-            for (int i = 0; i < len; ++i)
-            {
-                ++count_letters[s[i] - 'a'];
-            }
+        // reusable count of letters in the current substring
+        vector<int> count_letters(26);
+        for (char c : s)
+            ++count_letters[c - 'a'];
 
+        // the left-to-right and right-to-left checks are inspired by cocktail shaker sort
+        int len = s.size();
+        for (; len > 0;)
+        {
+            // check if the substring of length `len` is balanced
+            // from left to right
             for (int i = 0; i + len < s.size(); ++i)
             {
                 if (isBalanced(count_letters))
@@ -26,6 +29,23 @@ public:
             }
             if (isBalanced(count_letters))
                 return len;
+
+            --count_letters[s[s.size() - len--] - 'a'];
+            if (len == 0)
+                break;
+
+            // check if the substring of length `len` is balanced
+            // from right to left
+            for (int i = s.size() - len - 1; i >= 0; --i)
+            {
+                if (isBalanced(count_letters))
+                    return len;
+                ++count_letters[s[i] - 'a'];
+                --count_letters[s[i + len] - 'a'];
+            }
+            if (isBalanced(count_letters))
+                return len;
+            --count_letters[s[--len] - 'a'];
         }
         return 0;
     }
