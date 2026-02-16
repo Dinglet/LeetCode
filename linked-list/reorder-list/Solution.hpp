5aa3c965241733d8cbb259ee334073a052ce04d5
Huang, Shien-Ting <10545636+Dinglet@users.noreply.github.com>
2026-02-16 16:49:13 +0800

    Improve space efficiency by replacing stack with reverseList

--- linked-list/reorder-list/Solution.hpp
+++ linked-list/reorder-list/Solution.hpp
@@ -20,25 +20,43 @@ public:
     {
         if (!head)
             return;
-        stack<ListNode *> nodes;
+
+        int length = 0;
         for (auto node = head; node; node = node->next)
-        {
-            nodes.push(node);
-        }
-        int length = nodes.size();
-        int t = length / 2;
+            ++length;
 
+        // reverse `n_tail` nodes
+        const int n_tail = length / 2;
+        ListNode *tail = head;
+        for (int step = length - n_tail; step--;)
+            tail = tail->next;
+        tail = reverseList(tail);
+
+        // insert reversed nodes into the list
         auto node = head; // 1
-        while (t--)
+        for (int step = n_tail; step--;)
         {
-            ListNode *next_node = node->next;  // 2
-            ListNode *back_node = nodes.top(); // n
-            nodes.pop();
+            ListNode *next_node = node->next; // 2
+            ListNode *next_tail = tail->next; // n-1
 
-            node->next = back_node;      // 1 -> n
-            back_node->next = next_node; // 1 -> n -> 2
-            node = next_node;            // 2
+            node->next = tail;      // 1 -> n
+            tail->next = next_node; // 1 -> n -> 2
+            node = next_node;       // 2
+            tail = next_tail;       // n-1
         }
         node->next = nullptr;
+    }
+
+    ListNode *reverseList(ListNode *head)
+    {
+        ListNode *prev_node = nullptr;
+        while (head)
+        {
+            ListNode *next_node = head->next;
+            head->next = prev_node;
+            prev_node = head;
+            head = next_node;
+        }
+        return prev_node;
     }
 };
