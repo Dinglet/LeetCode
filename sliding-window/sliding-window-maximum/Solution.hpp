606b69956d558082f50e3a7c6a01dc8f937efed6
Huang, Shien-Ting <10545636+Dinglet@users.noreply.github.com>
2026-03-03 10:50:01 +0800

    Add a dynamic programming solution to Sliding Window Maximum problem

--- sliding-window/sliding-window-maximum/Solution.hpp
+++ sliding-window/sliding-window-maximum/Solution.hpp
@@ -9,6 +9,11 @@ class Solution
 public:
     vector<int> maxSlidingWindow(vector<int> &nums, int k)
     {
+        return maxSlidingWindowDP(nums, k);
+    }
+
+    vector<int> maxSlidingWindowHeap(vector<int> &nums, int k)
+    {
         priority_queue<pair<int, int>> max_heap;
         for (int i = 0; i + 1 < k; ++i)
         {
@@ -21,6 +26,31 @@ public:
             while (max_heap.top().second <= i)
                 max_heap.pop();
             output.push_back(max_heap.top().first);
+        }
+        return output;
+    }
+
+    vector<int> maxSlidingWindowDP(vector<int> &nums, int k)
+    {
+        const int n = nums.size();
+        vector<int> left_max(n);
+        vector<int> right_max(n);
+        left_max.front() = nums.front();
+        right_max.back() = nums.back();
+        for (int i = 1; i < n; ++i)
+        {
+            if (i % k != 0)
+                left_max[i] = max(left_max[i], left_max[i - 1]);
+        }
+        for (int i = n - 1; i--;)
+        {
+            if (i % k != 0)
+                right_max[i] = max(right_max[i], right_max[i + 1]);
+        }
+        vector<int> output;
+        for (int i = 0; i + k <= n; ++i)
+        {
+            output.push_back(max(right_max[i], left_max[i + k - 1]));
         }
         return output;
     }
