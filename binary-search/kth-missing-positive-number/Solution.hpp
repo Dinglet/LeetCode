64ad47116b29083684c7ae92da67cb5586d7dbd7
Huang, Shien-Ting <10545636+Dinglet@users.noreply.github.com>
2026-04-08 15:42:35 +0800

    Add a binary search solution to Kth Missing Positive Number problem

--- binary-search/kth-missing-positive-number/Solution.hpp
+++ binary-search/kth-missing-positive-number/Solution.hpp
@@ -8,6 +8,23 @@ class Solution
 public:
     int findKthPositive(vector<int> &arr, int k)
     {
+        // using binary search to find the kth missing number
+        int first = 0, last = arr.size();
+        while (first < last)
+        {
+            int mid = first + (last - first) / 2;
+            // number of missing numbers before arr[mid] is (arr[mid] - mid - 1)
+            if (arr[mid] - mid - 1 < k)
+                first = mid + 1;
+            else
+                last = mid;
+        }
+        return first + k;
+    }
+
+private:
+    int findKthPositive0(vector<int> &arr, int k)
+    {
         // check for missing numbers starting from 1
         int missing_number = 1;
         for (int i = 0; i < arr.size(); ++i)
